684c88c79dca928882f015c262f961a30be4d332
Ali Unal <0033484@gmail.com>
2020-03-12 23:27:04 +0300

    GameManager added

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake() {
+        if(Instance != null && Instance != this) {
+            Destroy(gameObject);
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        SetFrameRate();
+    }
+
+    private void SetFrameRate() {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = 60;
+    }
+}
