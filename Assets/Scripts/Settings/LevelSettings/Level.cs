f051234c5dcc11dd2e0b96a0c48eaf0d5601e08c
Ali Unal <0033484@gmail.com>
2020-03-14 15:51:07 +0300

    LevelSettings added

---
+++ Assets/Scripts/Settings/LevelSettings/Level.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[CreateAssetMenu(fileName = "Level0", menuName = "Level/New Level", order = 3)]
+public class Level : ScriptableObject {
+    [Header("Level Properties")]
+    [SerializeField] private float length;
+
+    [Header("Theme")]
+    [SerializeField] [ReorderableList] private List<Color> groundColors;
+    [SerializeField] [ReorderableList] private List<Color> backgroundColors;
+
+    [SerializeField] [ReorderableList] private List<Color> playerColor;
+    [SerializeField] [ReorderableList] private List<Color> obstacleColor;
+}
---
+++ Assets/Scripts/Settings/LevelSettings/LevelSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#pragma warning disable 0649
+public class LevelSettings : MonoSingleton<LevelSettings> {
+    public static Level Level {
+        get {
+            return Instance.level;
+        }
+    }
+
+    public Level level;
+}
