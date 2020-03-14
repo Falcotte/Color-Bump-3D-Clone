ff4ba83a289cfec38c5cc0f5c711c46a6a63b430
Ali Unal <0033484@gmail.com>
2020-03-14 04:07:56 +0300

    DOTween added & obstacleEditor implemented

---
+++ Assets/Scripts/Entities/Editor/ObstacleEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Obstacle))]
+public class ObstacleEditor : Editor {
+    private Renderer visualRenderer;
+    private Obstacle obstacle;
+
+    private void OnEnable() {
+        obstacle = (Obstacle)target;
+        visualRenderer = obstacle.VisualRenderer;
+    }
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        visualRenderer.sharedMaterial.color = obstacle.VisualColor;
+    }
+}
--- Assets/Scripts/Entities/Obstacle.cs
+++ Assets/Scripts/Entities/Obstacle.cs
@@ -2,13 +2,19 @@
 
 #pragma warning disable 0649
 public class Obstacle : MonoBehaviour {
-    [SerializeField] private Color color;
+    [SerializeField] private Color visualColor;
+    public Color VisualColor => visualColor;
+
+    [SerializeField] private Renderer visualRenderer;
+    public Renderer VisualRenderer => visualRenderer;
+
     [SerializeField] private bool isLethal;
 
     private void OnCollisionEnter(Collision collision) {
         if(collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
             if(isLethal) {
                 Debug.Log("Game over");
+                GameManager.Instance.CurrentState = GameStates.LevelEnd;
             }
         }
     }
